9eddeaa5acd5aa0f6149570ed22cf55c9ddc86c7
forHoneywell <37234863+forHoneywell@users.noreply.github.com>
2018-03-10 05:52:08 +0000

    Initial commit

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication5.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Test" << endl;
+
+
+	system("pause");
+    return 0;
+}
+
